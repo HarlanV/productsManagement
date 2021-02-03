369aa3cb9e72ba3c2d8ba63d770d784e3bf61804
HarlanV <harlan.eq@gmail.com>
2021-02-03 12:05:20 -0300

    Remoção do Construct do CategoryService

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -4,17 +4,9 @@ namespace App\Services;
 
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Response;
 
 class CategoryService
 {
-    public Category $category;
-
-    public function __construct(Category $category)
-    {
-        $this->category = $category;
-    }
-
     /**
      * Comunicates with the Model type and creates a new type
      * 
